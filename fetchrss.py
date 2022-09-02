030939de1ef14027864777a0778f9990993a6f32
Thomas Morton <thomasandrewmorton@gmail.com>
2022-09-02 14:39:39 +0100

    Renamed file

--- fetchrss.py
+++ fetchrss.py
@@ -7,8 +7,9 @@ from copy import deepcopy
 
 
 class SkFetchRss:
+    _lastThreads = []
     threads = []
-    lastThreads = []
+    newThreads = []
 
     def __init__(self, cookies):
         self.headers = {"cookie":cookies}
@@ -23,6 +24,19 @@ class SkFetchRss:
         self.staffappFeed = feedparser.parse("https://shadowkingdom.org/forums/staff-applications.43.rss")
 
 
+    def _parse(self, title, content):
+        # Remove HTML tags
+        tags = r"<[^<]+?>"
+        parsed = re.sub(tags, "", content)
+
+        # Unescape any HTML escaped characters
+        parsed = html.unescape(parsed)
+
+        # Remove title suffix in content
+        parsed = parsed.replace("\n\n" + title, "")
+        return parsed
+
+
     def _getMod(self, content):
         # Remove HTML tags
         tags = r"<[^<]+?>"
@@ -30,7 +44,7 @@ class SkFetchRss:
 
         # Unescape any HTML escaped characters
         parsed = html.unescape(parsed)
-        
+
         # Get the input after the field about who made the punishment
         name = ""
         return name
@@ -38,7 +52,7 @@ class SkFetchRss:
 
     def _getThreads(self):
         # Get difference between the threads fetched now and previously to tell what is new
-        self.lastThreads = deepcopy(self.threads)
+        self._lastThreads = deepcopy(self.threads)
 
         for item in self.serverFeed.entries:
             self.threads.append({"link":item.id,
@@ -46,14 +60,18 @@ class SkFetchRss:
                                  "title":item.title,
                                  "author":item.author,
                                  "date":item.published,
-                                 "moderator":self._getMod(item.content[0].get("value"))})
+                                 "moderator":self._getMod(item.content[0].get("value")),
+                                 "content":self._parse(item.title, item.content[0].get("value"))
+                                })
 
         for item in self.discordFeed.entries:
             self.threads.append({"link":item.id,
                                  "type":"discord-appeal",
                                  "title":item.title,
                                  "author":item.author,
-                                 "date":item.published})
+                                 "date":item.published,
+                                 "content":self._parse(item.title, item.content[0].get("value"))
+                                })
 
         # Skipping the first thread/entry in staff applications. It is a guide
         post = 0
@@ -63,18 +81,22 @@ class SkFetchRss:
                                      "type":"staff-app",
                                      "title":item.title,
                                      "author":item.author,
-                                     "date":item.published})
+                                     "date":item.published,
+                                     "content":self._parse(item.title, item.content[0].get("value"))
+                                    })
             post += 1
 
     def run(self):
         while True:
             self._getThreads()
 
-            if len(self.threads) > len(self.lastThreads):
-                print("Alert! New thread(s)!")
+            if len(self.threads) > len(self._lastThreads):
+                print("New threads: %d" % (len(self.threads) - len(self._lastThreads)))
                 
+                self.newThreads.clear()
                 for item in self.threads:
-                    if item not in self.lastThreads:
-                        print(item.get("link"))
+                    if item not in self._lastThreads:
+                        self.newThreads.append(item)
 
+            # Scan for new posts every 10 minutes
             time.sleep(600)
