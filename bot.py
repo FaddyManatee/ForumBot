2c1ff8e132708cf8ba9ea04eeb84831cf5c90a5c
Thomas Morton <sc21tam@leeds.ac.uk>
2023-01-01 01:26:59 +0000

    Embeds and further data extraction

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 # https://discordpy.readthedocs.io/en/latest/index.html
 import os
 import discord
+import requests
 import fetchrss as sk
+from math import floor
+from datetime import datetime as dt
 from discord import app_commands
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
@@ -15,7 +18,7 @@ intents.members = True
 bot = commands.Bot(command_prefix="sudo ", intents=intents, help_command=None)
 
 # If bot stops working, check if cookies in .env need updating first
-rss = sk.FetchRss(os.getenv("COOKIES"))
+rss = sk.FetchRss(os.getenv("COOKIE"), os.getenv("USER_AGENT"))
 
 
 @bot.event
@@ -71,7 +74,7 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
-@tasks.loop(minutes=5.0)
+@tasks.loop(minutes=30.0)
 async def fetcher():
     detected = rss.run()
     print(detected)
@@ -79,7 +82,55 @@ async def fetcher():
     if (detected > 0):
         channel = bot.get_channel(int(os.getenv("CHANNEL_ID")))
         threads = rss.getNewThreads()
+
         await channel.send("New threads: {}".format(len(threads)))
+        
+        for thread in threads:
+            embed = discord.Embed(title=thread["title"])
+            
+            avatar = thread["author_avatar"]
+
+            if "xenforo" in avatar:
+                embed.set_author(name=thread["author"], url="https://shadowkingdom.org/members/{}.{}/".format(thread["author_formatted"], thread["author_id"]), icon_url="https://i.postimg.cc/cJQnqTsS/unknown.jpg".format(thread["player"]))
+            else:
+                embed.set_author(name=thread["author"], url="https://shadowkingdom.org/members/{}.{}/".format(thread["author_formatted"], thread["author_id"]), icon_url=avatar)
+
+
+            thumbnail = requests.get("https://minotar.net/helm/{}.png".format(thread["player"]))
+
+
+            if thumbnail.status_code != 200:
+                embed.set_thumbnail(url="https://minotar.net/helm/{}.png".format(os.getenv("UNKNOWN_IGN")))
+            else:
+                embed.set_thumbnail(url=thumbnail.url)
+
+
+            embed.add_field(name="Player", value=discord.utils.escape_markdown(thread["player"]))
+            
+
+            if thread["type"] == "server-appeal":
+                embed.description = "Server punishment appeal"
+                embed.color = discord.Color.from_str("#ff2828")
+                embed.add_field(name="Staff member", value=thread["moderator"])
+
+            elif thread["type"] == "discord-appeal":
+                embed.color = discord.Color.from_str("#ff2828")
+                embed.description = "Discord punishment appeal"
+
+            elif thread["type"] == "staff-app":
+                embed.color = discord.Color.from_str("#00f343")
+                embed.description = "Staff application"
+
+            embed.add_field(name="Link", value=thread["link"], inline=False)
+            time_spanned = floor((dt.now() - thread["time"]).total_seconds() / 3600)
+
+
+            if time_spanned >= 24:
+                embed.set_footer(text=str(floor(time_spanned / 24)) + "d ago")
+            else:
+                embed.set_footer(text=str(time_spanned) + "h ago")
+
+            await channel.send(embed=embed)
 
 
 bot.run(os.getenv("TOKEN"))
--- fetchrss.py
+++ fetchrss.py
@@ -1,7 +1,10 @@
 # https://feedparser.readthedocs.io/en/latest/
 import feedparser
 import re
+import requests
 import html
+from datetime import datetime as dt
+from bs4 import BeautifulSoup
 
 
 class FetchRss:
@@ -9,77 +12,158 @@ class FetchRss:
     _threads = []
     _newThreads = []
 
-    def __init__(self, cookies):
-        self.headers = {"cookie": cookies}
+    def __init__(self, cookie, agent):
+        self.cookie = {"cookie": cookie}
 
         # Three pages of interest
         self.serverFeed = feedparser.parse("https://shadowkingdom.org/forums/ban-mute-appeals.15.rss", 
-                                            request_headers=self.headers)
+                                            request_headers=self.cookie)
 
         self.discordFeed = feedparser.parse("https://shadowkingdom.org/forums/discord-ban-mute-appeals.49.rss", 
-                                             request_headers=self.headers)
+                                             request_headers=self.cookie)
 
         self.staffappFeed = feedparser.parse("https://shadowkingdom.org/forums/staff-applications.43.rss")
 
 
-    def _parse(self, title, content):
+    def _parse(self, content):
         # Remove HTML tags
         tags = r"<[^<]+?>"
         parsed = re.sub(tags, "", content)
 
-        # Remove title suffix in content
-        parsed = parsed.replace("\n\n" + title, "")
+        parsed = html.unescape(parsed)
         return parsed
+
+    
+    def _formatAuthor(self, author):
+        author = author.lower()
+        author = author.replace(" ", "-")
+        return author
+
+
+    # https://stackoverflow.com/a/48600278
+    def _findNumber(self, text, c):
+        return re.findall(r"%s(\d+)" % c, text)
+
+
+    def _getAuthorID(self, soup, author):
+        authors = soup.find_all(class_="username")
+        
+        author = self._formatAuthor(author)
+        author = author + "."
+
+        for entry in authors:
+            if author in str(entry):
+                id = self._findNumber(str(entry), ".")[0]
+                return id
+
+
+    def _getAuthorAvatar(self, soup):
+        author_avatars = soup.find_all("img")
+        author_avatars.remove(author_avatars[0])  # Remove hidden img avatar of auth user
+        
+        for entry in author_avatars:
+            if "96" in str(entry):
+                return "https://shadowkingdom.org/{}".format(entry["src"])
 
 
     def _getMod(self, content):
-        # Remove HTML tags
-        tags = r"<[^<]+?>"
-        parsed = re.sub(tags, "", content)
-
-        # Unescape any HTML escaped characters
-        parsed = html.unescape(parsed)
-
         # Get the input after the field about who made the punishment
-        lines = list(filter(None, parsed.splitlines()))
+        lines = iter(list(filter(None, content.splitlines())))
         for line in lines:
             if line.startswith("Staff member who issued your punishment:"):
-                return line.replace("Staff member who issued your punishment:", "").strip()
+                header = line.replace("Staff member who issued your punishment:", "").strip()
+
+                # Try the next line if the data was not inline with header 
+                if header.isspace() or len(header) == 0:
+                    l = next(lines)
+                    header = l.replace("Staff member who issued your punishment:", "").strip()
+                    
+                return header
+        return None
 
 
-    def _formatThreads(self):
+    def _getPlayer(self, content, status):
+        sentence = ""
+        if status == "banned":
+            sentence = "Your in-game name:"
+        elif status == "applying":
+            sentence = "Your current IGN:"
+
+        # Get the input after the field about who was punished
+        lines = iter(list(filter(None, content.splitlines())))
+        for line in lines:
+            if line.startswith(sentence):
+                header = line.replace(sentence, "").strip()
+
+                # Try the next line if the data was not inline with header 
+                if header.isspace() or len(header) == 0:
+                    l = next(lines)
+                    header = l.replace(sentence, "").strip()
+
+                return header
+        return None
+
+
+    def _fetch(self):
         self._lastThreads.extend(self._threads)
         self._threads.clear()
 
         for item in self.serverFeed.entries:
+            thread = requests.get(item.id, cookies=self.cookie)
+            soup = BeautifulSoup(thread.content, "html.parser")
+            content = str(soup.find("blockquote"))
+            content = self._parse(content)
+
             self._threads.append({"link": item.id,
                                   "type": "server-appeal",
                                   "title": item.title,
+                                  "author_id": self._getAuthorID(soup, item.author),
                                   "author": item.author,
-                                  "date": item.published,
-                                  "moderator": self._getMod(item.content[0].get("value")),
-                                  "content": self._parse(item.title, item.content[0].get("value"))
+                                  "author_formatted": self._formatAuthor(item.author),
+                                  "author_avatar": self._getAuthorAvatar(soup),
+                                  "time": dt.strptime(item.published[:-6], "%a, %d %b %Y %H:%M:%S"),
+                                  "player": self._getPlayer(content, "banned"),
+                                  "moderator": self._getMod(content),
+                                  "content": content
                                 })
 
         for item in self.discordFeed.entries:
+            thread = requests.get(item.id, cookies=self.cookie)
+            soup = BeautifulSoup(thread.content, "html.parser")
+            content = str(soup.find("blockquote"))
+            content = self._parse(content)
+
             self._threads.append({"link": item.id,
                                   "type": "discord-appeal",
                                   "title": item.title,
+                                  "author_id": self._getAuthorID(soup, item.author),
                                   "author": item.author,
-                                  "date": item.published,
-                                  "content": self._parse(item.title, item.content[0].get("value"))
+                                  "author_formatted": self._formatAuthor(item.author),
+                                  "author_avatar": self._getAuthorAvatar(soup),
+                                  "time": dt.strptime(item.published[:-6], "%a, %d %b %Y %H:%M:%S"),
+                                  "player": self._getPlayer(content, "banned"),
+                                  "content": content
                                 })
 
         # Skipping the first thread/entry in staff applications. It is a guide
         post = 0
         for item in self.staffappFeed.entries:
             if post != 0:
+                thread = requests.get(item.id, cookies=self.cookie)
+                soup = BeautifulSoup(thread.content, "html.parser")
+                content = str(soup.find("blockquote"))
+                content = self._parse(content)
+
                 self._threads.append({"link": item.id,
                                       "type": "staff-app",
                                       "title": item.title,
+                                      "author_id": self._getAuthorID(soup, item.author),
                                       "author": item.author,
-                                      "date": item.published,
-                                      "content": self._parse(item.title, item.content[0].get("value"))
+                                      "author_formatted": self._formatAuthor(item.author),
+                                      "author_avatar": self._getAuthorAvatar(soup),
+                                      "time": dt.strptime(item.published[:-6], "%a, %d %b %Y %H:%M:%S"),
+                                      "player": self._getPlayer(content, "applying"),
+                                      "content": content
                                     })
             post += 1
 
@@ -105,7 +189,7 @@ class FetchRss:
 
 
     def run(self):
-        self._formatThreads()
+        self._fetch()
 
         if len(self._threads) > len(self._lastThreads):            
             self._newThreads.clear()
