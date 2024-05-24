e524174358edaef63ce8fcfb712b7102e976060b
Thomas Morton <tommeltot@icloud.com>
2024-05-24 18:52:24 +0100

    Further proof of concept

--- src/concept.py
+++ src/concept.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from datetime import datetime as dt
 from dotenv import load_dotenv
 from urllib.parse import urljoin
 import os
@@ -27,4 +28,31 @@ thread = requests.get(thread_links[0], headers=header)
 soup = BeautifulSoup(thread.text, "html.parser")
 posts = soup.find_all("div", attrs={"id": re.compile(r"post-\d+")})
 print(posts)
+################################################################################
+
+
+##################### EXTRACT DETAILS FROM A POST ##############################
+soup_a = BeautifulSoup(str(posts[0]), "html.parser")
+soup_b = BeautifulSoup(str(posts[1]), "html.parser")
+
+# Author's name.
+username = soup_a.find(class_="username")
+print(username.text)
+
+# Author's profile picture (default avatar).
+avatar = soup_a.find(class_="avatar-img")
+print(avatar.get("src"))
+
+# Author's profile picture (custom avatar).
+avatar = soup_b.find(class_="avatar-img")
+print(urljoin(url, avatar.get("src")))
+
+# DateTime posted.
+time = soup_a.find("span", attrs={"data-toggle": "tooltip"})
+print(dt.strptime(time.get("data-original-title"), "%d %b %Y, %H:%M"))
+
+# Post content.
+soup = BeautifulSoup(str(posts[1]), "html.parser")
+content = soup.find(class_="forum-post-content")
+print(content.text)
 ################################################################################
